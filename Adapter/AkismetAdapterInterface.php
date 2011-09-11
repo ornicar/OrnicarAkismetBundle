682d1bab83d4bf66b26e67328189d333fb8ac629
ornicar <thibault.duplessis@gmail.com>
2011-09-11 20:29:19 +0200

    Fix AkismetAdapterInterface

--- Adapter/AkismetAdapterInterface.php
+++ Adapter/AkismetAdapterInterface.php
@@ -4,5 +4,11 @@ namespace Ornicar\AkismetBundle\Adapter;
 
 interface AkismetAdapterInterface
 {
-
+    /**
+     * Tells if the data looks like spam
+     *
+     * @param array $data
+     * @return boolean
+     */
+    function isSpam(array $data);
 }
