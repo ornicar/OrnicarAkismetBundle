0e3ef258d791f41751f55fe6e6ae31d0eae81e23
Saša Stamenković <umpirsky@gmail.com>
2018-07-20 19:08:49 +0200

    Not implementing "Symfony\Component\Config\Definition\ConfigurationInterface" in the extension configuration class "Ornicar\AkismetBundle\DependencyInjection\Configuration" is deprecated since Symfony 4.1

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Ornicar\AkismetBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\NodeBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 /**
  * This class Configuration the configuration information for the bundle
@@ -11,27 +12,23 @@ use Symfony\Component\Config\Definition\Builder\TreeBuilder;
  * This information is solely responsible for how the different configuration
  * sections are normalized, and merged.
  */
-class Configuration
+class Configuration implements ConfigurationInterface
 {
     /**
      * Generates the configuration tree.
      *
      * @return NodeInterface
      */
-    public function getConfigTree()
+    public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-
-        $treeBuilder->root('ornicar_akismet', 'array')
+        return (new TreeBuilder())->root('ornicar_akismet', 'array')
             ->children()
                 ->scalarNode('url')->isRequired()->end()
                 ->scalarNode('api_key')->isRequired()->end()
                 ->scalarNode('service')->defaultValue('ornicar_akismet.akismet_real')->end()
                 ->scalarNode('adapter')->defaultValue('ornicar_akismet.adapter.guzzle')->end()
-                ->scalarNode('throw_exceptions')->defaultValue('%kernel.debug%')
+                ->scalarNode('throw_exceptions')->defaultValue('%kernel.debug%')->end()
             ->end()
         ->end();
-
-        return $treeBuilder->buildTree();
     }
 }
